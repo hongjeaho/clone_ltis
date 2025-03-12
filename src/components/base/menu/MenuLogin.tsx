078ce440f5eb821ef375f9dc59f32d26f8379271
hongjeaho <teil83@kapanet.co.kr>
2025-03-12 17:10:28 +0900

    로그인 했을경우 로그인 버튼 숨김 처리

--- src/components/base/menu/MenuLogin.tsx
+++ src/components/base/menu/MenuLogin.tsx
@@ -1,8 +1,15 @@
 import { Link } from 'react-router-dom'
+import { useRecoilValue } from 'recoil'
+
+import { isLoginSelector } from '@/store/user'
 
 import styles from './MenuLogin.module.css'
 
 const MenuLogin: React.FC = () => {
+  const isLogin = useRecoilValue<boolean>(isLoginSelector)
+
+  if (isLogin) return null
+
   return (
     <div className={styles.login}>
       <ul>
