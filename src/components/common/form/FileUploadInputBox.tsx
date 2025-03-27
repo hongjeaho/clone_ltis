1d16b32be186a6940de67c45c3eed7b396bc9a5b
hongjeaho <teil83@kapanet.co.kr>
2025-03-27 13:31:43 +0900

    파일 업로드 rules 기능 추가

--- src/components/common/form/FileUploadInputBox.tsx
+++ src/components/common/form/FileUploadInputBox.tsx
@@ -1,19 +1,22 @@
 import styled from '@emotion/styled/macro'
-import { Button, InputBase, Paper } from '@mui/material'
+import { Box, Button, FormHelperText, InputBase, Paper } from '@mui/material'
 import React from 'react'
-import { type Control, Controller } from 'react-hook-form'
+import { type Control, Controller, type FieldError } from 'react-hook-form'
 import { FaFileUpload } from 'react-icons/fa'
 
 interface FileUploadInputBoxProps {
   control: Control<any>
   id: string
+  rules?: Record<string, any>
+  error?: FieldError
 }
 
-const FileUploadInputBox: React.FC<FileUploadInputBoxProps> = ({ id, control }) => {
+const FileUploadInputBox: React.FC<FileUploadInputBoxProps> = ({ id, control, error, rules }) => {
   return (
     <Controller
       name={id}
       control={control}
+      rules={rules}
       render={({ field }) => (
         <div>
           <Paper
@@ -24,29 +27,34 @@ const FileUploadInputBox: React.FC<FileUploadInputBoxProps> = ({ id, control }) 
               width: 400,
             }}
           >
-            <InputBase
-              sx={{ ml: 1, flex: 1 }}
-              value={field?.value ? field.value.name : ''}
-              placeholder="파일을 업로드 해주세요"
-              inputProps={{ 'aria-label': '파일을 업로드 해주세요' }}
-              disabled
-            />
-            <Button
-              component="label"
-              role={undefined}
-              variant="contained"
-              tabIndex={-1}
-              startIcon={<FaFileUpload />}
-            >
-              업로드
-              <VisuallyHiddenInput
-                type="file"
-                onChange={event => {
-                  const file = event.target.files?.[0]
-                  field.onChange(file)
-                }}
-              />
-            </Button>
+            <Box sx={{ display: 'flex', flexDirection: 'column', width: '100%' }}>
+              <Box sx={{ textAlign: 'start', display: 'flex' }}>
+                <InputBase
+                  sx={{ ml: 1, flex: 1 }}
+                  value={field?.value ? field.value.name : ''}
+                  placeholder="파일을 업로드 해주세요"
+                  inputProps={{ 'aria-label': '파일을 업로드 해주세요' }}
+                  error={!!error}
+                  disabled
+                />
+                <Button
+                  component="label"
+                  variant="contained"
+                  tabIndex={-1}
+                  startIcon={<FaFileUpload />}
+                >
+                  업로드
+                  <VisuallyHiddenInput
+                    type="file"
+                    onChange={event => {
+                      const file = event.target.files?.[0]
+                      field.onChange(file)
+                    }}
+                  />
+                </Button>
+              </Box>
+              <FormHelperText sx={{ color: 'red' }}>{error?.message}</FormHelperText>
+            </Box>
           </Paper>
         </div>
       )}
